db8e9dfbf001612ce8190b8c5b7c064d1e254d41
scott.walker <scott.walker@ODEWMJPWNHD.ectorcountyisd.org>
2015-03-24 19:54:52 -0500

    update

--- src/bogglecalc/Edge.java
+++ src/bogglecalc/Edge.java
@@ -9,45 +9,47 @@ package bogglecalc;
  *
  * @author scott.walker
  */
-public class Edge {
+public enum Edge {
 
-    private final byte node1, node2;
-
-    public Edge(byte a, byte b) {
-        node1 = a;
-        node2 = b;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 73 * hash + this.node1;
-        hash = 73 * hash + this.node2;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Edge)) {
-            return false;
-        }
-        Edge e = (Edge) o;
-        final byte a = e.getNode1();
-        final byte b = e.getNode2();
-        return (a == this.node1 && b == this.node2)
-                || (b == this.node1 && a == this.node2);
-    }
-
-    byte getNode1() {
-        return node1;
-    }
-
-    byte getNode2() {
-        return node2;
-    }
-
-    @Override
-    public String toString() {
-        return node1 + "-" + node2;
-    }
+//public class Edge {
+//
+//    private final byte node1, node2;
+//
+//    public Edge(byte a, byte b) {
+//        node1 = a;
+//        node2 = b;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int hash = 7;
+//        hash = 73 * hash + this.node1;
+//        hash = 73 * hash + this.node2;
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (!(o instanceof Edge)) {
+//            return false;
+//        }
+//        Edge e = (Edge) o;
+//        final byte a = e.getNode1();
+//        final byte b = e.getNode2();
+//        return (a == this.node1 && b == this.node2)
+//                || (b == this.node1 && a == this.node2);
+//    }
+//
+//    byte getNode1() {
+//        return node1;
+//    }
+//
+//    byte getNode2() {
+//        return node2;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return node1 + "-" + node2;
+//    }
 }
